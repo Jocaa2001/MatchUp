f3ef93bc3c8e6eb61045df3dba9d3309d30971a9
Jocaa2001 <jovanvukovic2001@gmail.com>
2026-01-18 18:22:15 +0100

    initial commit

---
+++ Backend/matchup/src/test/java/com/matchup/MatchUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matchup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
